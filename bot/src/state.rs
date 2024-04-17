d35e93ca0cc8073b727343503331b58d4de82603
007 <dr007cc@gmail.com>
2024-04-18 00:18:08 +0330

    fix: update the bot state

--- bot/src/state.rs
+++ bot/src/state.rs
@@ -1,7 +1,7 @@
-use crate::types::{ChitChat, HR, PurchaseKind};
-
-pub async fn state_buy(chit: &ChitChat) -> HR {
-    let state = chit.get_or_default().await?;
-
-    Ok(())
-}
+// use crate::types::{ChitChat, HR, PurchaseKind};
+//
+// pub async fn state_buy(bot: chit: &ChitChat) -> HR {
+//     let state = chit.get_or_default().await?;
+//
+//     Ok(())
+// }
