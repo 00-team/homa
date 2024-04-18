dbe9befc2d6b7a90378b334277e2761c1256fd88
007 <dr007cc@gmail.com>
2024-04-19 01:41:52 +0330

    feat: update auth

--- web/src/api/auth.rs
+++ web/src/api/auth.rs
@@ -1,61 +1,32 @@
-use actix_multipart::form::MultipartForm;
 use actix_web::error::{self, ErrorBadRequest};
-use actix_web::web::{Data, Json, Query};
-use actix_web::{
-    delete, get, patch, post, put, HttpResponse, Responder, Scope,
-};
+use actix_web::http::header;
+use actix_web::http::StatusCode;
+use actix_web::web::{Data, Json, Query, Redirect};
+use actix_web::{get, post, HttpResponse, Responder, Scope};
 use hmac::{Hmac, Mac};
 use serde::{Deserialize, Serialize};
 use sha2::Sha256;
-// use sha2::{Digest, Sha512};
-use utoipa::{OpenApi, ToSchema};
+use utoipa::{IntoParams, OpenApi, ToSchema};
 
 use crate::config::config;
-// use crate::api::verification::verify;
-// use crate::config::Config;
-// use crate::docs::UpdatePaths;
-// use crate::models::{
-//     Action, Address, JsonStr, ListInput, Response, Transaction, UpdatePhoto,
-//     User,
-// };
-// use crate::utils::{
-//     get_random_bytes, get_random_string, remove_photo, save_photo, sql_unwrap,
-//     CutOff,
-// };
+use crate::config::Config;
+use crate::docs::UpdatePaths;
+use crate::models::{JsonStr, ListInput, Response, Transaction, User};
+use crate::utils::{get_random_bytes, get_random_string, remove_photo, CutOff};
 use crate::AppState;
 
 type Hmac256 = Hmac<Sha256>;
 
-// #[derive(OpenApi)]
-// #[openapi(
-//     tags((name = "api::user")),
-//     paths(
-//         login, user_get, user_update, user_update_photo,
-//         user_delete_photo, user_wallet_test, user_transactions_list
-//     ),
-//     components(schemas(
-//         User, LoginBody, UserUpdateBody, Address, UpdatePhoto, Transaction
-//     )),
-//     servers((url = "/auth")),
-//     modifiers(&UpdatePaths)
-// )]
-// pub struct ApiUserDoc;
-//
-// #[derive(Debug, Deserialize, ToSchema)]
-// struct LoginBody {
-//     phone: String,
-//     code: String,
-// }
-//
-// #[utoipa::path(
-//     post,
-//     request_body = LoginBody,
-//     responses(
-//         (status = 200, body = User),
-//         (status = 400, body = String)
-//     )
-// )]
-// /// Login
+#[derive(OpenApi)]
+#[openapi(
+    tags((name = "api::auth")),
+    paths(login_telegram),
+    components(schemas(LoginTelQuery)),
+    servers((url = "/auth")),
+    modifiers(&UpdatePaths)
+)]
+pub struct ApiAuthDoc;
+
 // #[post("/login/")]
 // async fn login(body: Json<LoginBody>, state: Data<AppState>) -> Response<User> {
 //     verify(&body.phone, &body.code, Action::Login, &state.sql).await?;
@@ -106,24 +77,38 @@ type Hmac256 = Hmac<Sha256>;
 //     Ok(Json(user))
 // }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, ToSchema, IntoParams)]
 pub struct LoginTelQuery {
     auth_date: u64,
     first_name: String,
     id: i64,
-    photo_url: String,
-    username: String,
+    last_name: Option<String>,
+    photo_url: Option<String>,
+    username: Option<String>,
     hash: String,
 }
 
-type Response<T> = Result<T, error::Error>;
-
+#[utoipa::path(
+    get,
+    params(LoginTelQuery),
+    responses((status = 302))
+)]
 #[get("/login-telegram/")]
-async fn login_telegram(q: Query<LoginTelQuery>) -> Response<String> {
-    let msg = format!(
-        "auth_date={}\nfirst_name={}\nid={}\nphoto_url={}\nusername={}",
-        q.auth_date, q.first_name, q.id, q.photo_url, q.username,
+async fn login_telegram(q: Query<LoginTelQuery>) -> impl Responder {
+    let mut msg = format!(
+        "auth_date={}\nfirst_name={}\nid={}",
+        q.auth_date, q.first_name, q.id
     );
+
+    if let Some(last_name) = &q.last_name {
+        msg += &("\nlast_name=".to_string() + last_name)
+    }
+    if let Some(photo_url) = &q.photo_url {
+        msg += &("\nphoto_url=".to_string() + photo_url)
+    }
+    if let Some(username) = &q.username {
+        msg += &("\nusername=".to_string() + username)
+    }
 
     let mut mac = Hmac256::new_from_slice(&config().bot_token_hash).unwrap();
     mac.update(msg.as_bytes());
@@ -133,7 +118,10 @@ async fn login_telegram(q: Query<LoginTelQuery>) -> Response<String> {
         return Err(ErrorBadRequest("invalid login credentials ❌"));
     }
 
-    Ok(format!("{q:#?}"))
+    Ok(HttpResponse::build(StatusCode::FOUND)
+        .insert_header((header::LOCATION, "/"))
+        .insert_header((header::AUTHORIZATION, "Bearer token"))
+        .finish())
 }
 
 pub fn router() -> Scope {
