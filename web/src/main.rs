af3f108ff306ac639f36b01e0b419bd0b43d23dc
007 <dr007cc@gmail.com>
2024-04-18 23:13:45 +0330

    fix: update web main

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,20 +1,22 @@
-use std::{env, os::unix::fs::PermissionsExt};
+use std::{fs::read_to_string, os::unix::fs::PermissionsExt};
 
-// use actix_files as af;
+use actix_files as af;
 use actix_web::{
     get,
-    // http::header::ContentType,
+    http::header::ContentType,
     middleware,
-    web::{scope, Data},
-    App,
-    HttpResponse,
-    HttpServer,
-    Responder,
+    web::{self, scope, Data},
+    App, HttpResponse, HttpServer, Responder,
 };
 use sqlx::{Pool, Sqlite, SqlitePool};
+use utoipa::OpenApi;
 
 mod api;
 mod config;
+mod docs;
+mod general;
+mod models;
+mod utils;
 
 pub struct AppState {
     pub sql: Pool<Sqlite>,
@@ -22,21 +24,63 @@ pub struct AppState {
 
 #[get("/")]
 async fn index() -> impl Responder {
-    // let result = read_to_string("dist/index.html")
-    //     .unwrap_or("err reading index.html".to_string());
-    // HttpResponse::Ok().content_type(ContentType::html()).body(result)
-    HttpResponse::Ok().body("hi from thora index")
+    let result = read_to_string("dist/index.html")
+        .unwrap_or("err reading index.html".to_string());
+    HttpResponse::Ok().content_type(ContentType::html()).body(result)
+}
+
+#[get("/openapi.json")]
+async fn openapi() -> impl Responder {
+    let mut doc = docs::ApiDoc::openapi();
+    doc.merge(api::auth::ApiAuthDoc::openapi());
+    // doc.merge(api::verification::ApiVerificationDoc::openapi());
+    // doc.merge(api::product::Doc::openapi());
+
+    // let mut admin_doc = ApiDoc::openapi();
+    // admin_doc.merge(admin::user::Doc::openapi());
+    // admin_doc.merge(admin::product::Doc::openapi());
+    //
+    // doc_add_prefix(&mut admin_doc, "/admin", false);
+    //
+    // doc.merge(admin_doc);
+
+    docs::doc_add_prefix(&mut doc, "/api", false);
+
+    HttpResponse::Ok().json(doc)
+}
+
+#[get("/rapidoc")]
+async fn rapidoc() -> impl Responder {
+    HttpResponse::Ok().content_type(ContentType::html()).body(
+        r###"<!doctype html>
+    <html><head><meta charset="utf-8"><style>rapi-doc {
+    --green: #00dc7d; --blue: #5199ff; --orange: #ff6b00;
+    --red: #ec0f0f; --yellow: #ffd600; --purple: #782fef; }</style>
+    <script type="module" src="/static/rapidoc.js"></script></head><body>
+    <rapi-doc spec-url="/openapi.json" persist-auth="true"
+    bg-color="#040404" text-color="#f2f2f2"
+    header-color="#040404" primary-color="#ec0f0f"
+    nav-text-color="#eee" font-size="largest"
+    allow-spec-url-load="false" allow-spec-file-load="false"
+    show-method-in-nav-bar="as-colored-block" response-area-height="500px"
+    show-header="false" schema-expand-level="1" /></body> </html>"###,
+    )
+}
+
+fn config_static(app: &mut web::ServiceConfig) {
+    if cfg!(debug_assertions) {
+        app.service(af::Files::new("/static", "./static"));
+        app.service(af::Files::new("/assets", "./dist/assets"));
+        app.service(af::Files::new("/record", "./record"));
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    dotenvy::from_path("../.secrets.env").expect("could not read secrets.env");
+    dotenvy::from_path("../.secrets.env").expect("could not read .secrets.env");
     pretty_env_logger::init();
 
-    let pool =
-        SqlitePool::connect(&env::var("DATABASE_URL").expect("DATABASE_URL was not found in env"))
-            .await
-            .expect("sqlite pool initialization failed");
+    let pool = SqlitePool::connect("sqlite://main.db").await.unwrap();
 
     sqlx::migrate!().run(&pool).await.expect("migration failed");
 
@@ -44,18 +88,11 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(middleware::Logger::new("%s %r %Ts"))
             .app_data(Data::new(AppState { sql: pool.clone() }))
+            .configure(config_static)
+            .service(openapi)
+            .service(rapidoc)
             .service(index)
-            .service(
-                scope("/api")
-                    .service(api::auth::router())
-                    // .service(api::product::router())
-                    // .service(api::verification::verification)
-                    // .service(
-                    //     scope("/admin")
-                    //         .service(admin::user::router())
-                    //         .service(admin::product::router()),
-                    // ),
-            )
+            .service(scope("/api").service(api::auth::router()))
     });
 
     let server = if cfg!(debug_assertions) {
