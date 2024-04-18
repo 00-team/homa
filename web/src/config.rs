cd0083e8b4790a7878542895656c0de1bd4dc8f3
007 <dr007cc@gmail.com>
2024-04-18 16:36:17 +0330

    feat: update the config

--- web/src/config.rs
+++ web/src/config.rs
@@ -6,6 +6,14 @@ pub struct Config {
     pub bot_token_hash: [u8; 32],
 }
 
+impl Config {
+    pub const RECORD_DIR: &'static str = "./records/";
+    pub const CODE_ABC: &'static [u8] = b"0123456789";
+    pub const TOKEN_ABC: &'static [u8] = b"!@#$%^&*_+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*_+";
+    pub const OFFER_TAX: f32 = 2.5;
+    pub const OFFER_TAX_MAX: f32 = 50_000.0;
+}
+
 pub fn config() -> &'static Config {
     static STATE: OnceLock<Config> = OnceLock::new();
     STATE.get_or_init(|| {
