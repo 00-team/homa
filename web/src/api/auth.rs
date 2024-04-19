62c463102a685d8755c6e55417def5b9405f182a
007 <dr007cc@gmail.com>
2024-04-19 22:45:35 +0330

    fix: update auth login

--- web/src/api/auth.rs
+++ web/src/api/auth.rs
@@ -5,14 +5,18 @@ use actix_web::web::{Data, Json, Query, Redirect};
 use actix_web::{get, post, HttpResponse, Responder, Scope};
 use hmac::{Hmac, Mac};
 use serde::{Deserialize, Serialize};
-use sha2::Sha256;
+use sha2::{Digest, Sha256, Sha512};
 use utoipa::{IntoParams, OpenApi, ToSchema};
 
 use crate::config::config;
 use crate::config::Config;
 use crate::docs::UpdatePaths;
-use crate::models::{JsonStr, ListInput, Response, Transaction, User};
-use crate::utils::{get_random_bytes, get_random_string, remove_photo, CutOff};
+use crate::models::{
+    AppErr, AppErrBadRequest, JsonStr, ListInput, Response, Transaction, User,
+};
+use crate::utils::{
+    get_random_bytes, get_random_string, now, remove_photo, save_photo, CutOff,
+};
 use crate::AppState;
 
 type Hmac256 = Hmac<Sha256>;
@@ -31,55 +35,16 @@ pub struct ApiAuthDoc;
 // async fn login(body: Json<LoginBody>, state: Data<AppState>) -> Response<User> {
 //     verify(&body.phone, &body.code, Action::Login, &state.sql).await?;
 //
-//     let token = get_random_string(Config::TOKEN_ABC, 69);
-//     let token_hashed = hex::encode(Sha512::digest(&token));
+
 //
-//     let result = sqlx::query_as! {
-//         User,
-//         "select * from users where phone = ?",
-//         body.phone
-//     }
-//     .fetch_one(&state.sql)
-//     .await;
-//
-//     let user: User = match result {
-//         Ok(mut v) => {
-//             v.token = token;
-//
-//             let _ = sqlx::query_as! {
-//                 User,
-//                 "update users set token = ? where id = ?",
-//                 token_hashed, v.id
-//             }
-//             .execute(&state.sql)
-//             .await;
-//
-//             v
-//         }
-//         Err(_) => {
-//             let result = sqlx::query_as! {
-//                 User,
-//                 "insert into users (phone, token) values(?, ?)",
-//                 body.phone, token_hashed
-//             }
-//             .execute(&state.sql)
-//             .await;
-//
-//             User {
-//                 phone: body.phone.clone(),
-//                 token,
-//                 id: result.unwrap().last_insert_rowid(),
-//                 ..Default::default()
-//             }
-//         }
-//     };
+
 //
 //     Ok(Json(user))
 // }
 
 #[derive(Debug, Deserialize, ToSchema, IntoParams)]
 pub struct LoginTelQuery {
-    auth_date: u64,
+    auth_date: i64,
     first_name: String,
     id: i64,
     last_name: Option<String>,
@@ -94,17 +59,26 @@ pub struct LoginTelQuery {
     responses((status = 302))
 )]
 #[get("/login-telegram/")]
-async fn login_telegram(q: Query<LoginTelQuery>) -> impl Responder {
+async fn login_telegram(
+    q: Query<LoginTelQuery>, state: Data<AppState>,
+) -> Result<HttpResponse, AppErr> {
+    if q.auth_date < now() - 6 * 3600 {
+        return Err(AppErrBadRequest("login again"));
+    }
+
     let mut msg = format!(
         "auth_date={}\nfirst_name={}\nid={}",
         q.auth_date, q.first_name, q.id
     );
 
+    let mut name = q.first_name.clone();
     if let Some(last_name) = &q.last_name {
-        msg += &("\nlast_name=".to_string() + last_name)
+        msg += &("\nlast_name=".to_string() + last_name);
+        name += &(" ".to_string() + last_name);
     }
     if let Some(photo_url) = &q.photo_url {
-        msg += &("\nphoto_url=".to_string() + photo_url)
+        msg += &("\nphoto_url=".to_string() + photo_url);
+        save_photo(photo_url, q.id).await?;
     }
     if let Some(username) = &q.username {
         msg += &("\nusername=".to_string() + username)
@@ -115,8 +89,48 @@ async fn login_telegram(q: Query<LoginTelQuery>) -> impl Responder {
     let result = mac.finalize();
 
     if hex::encode(result.into_bytes()) != q.hash {
-        return Err(ErrorBadRequest("invalid login credentials ❌"));
+        return Err(AppErrBadRequest("invalid login credentials ❌"));
     }
+
+    let token = get_random_string(Config::TOKEN_ABC, 69);
+    let token_hashed = hex::encode(Sha512::digest(&token));
+
+    let result = sqlx::query_as! {
+        User,
+        "select * from users where id = ?",
+        q.id
+    }
+    .fetch_one(&state.sql)
+    .await;
+
+    let has_photo = q.photo_url.is_some();
+
+    match result {
+        Ok(user) => {
+            if user.auth_date == q.auth_date {
+                return Err(AppErrBadRequest("invalid login info"))
+            }
+
+            sqlx::query_as! {
+                User,
+                "update users set name = ?, auth_date = ?,
+                token = ?, photo = ? where id = ?",
+                name, q.auth_date, token_hashed, has_photo, user.id
+            }
+            .execute(&state.sql)
+            .await?;
+        }
+        Err(_) => {
+            sqlx::query_as! {
+                User,
+                "insert into users (id, name, auth_date, token, photo)
+                values(?, ?, ?, ?, ?)",
+                q.id, name, q.auth_date, token_hashed, has_photo
+            }
+            .execute(&state.sql)
+            .await?;
+        }
+    };
 
     Ok(HttpResponse::build(StatusCode::FOUND)
         .insert_header((header::LOCATION, "/"))
