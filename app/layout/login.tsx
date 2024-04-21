12f3977c5c02b9630ca2bc7ce00fb3f95f6859e3
007 <dr007cc@gmail.com>
2024-04-21 13:23:19 +0330

    feat: init login page

---
+++ app/layout/login.tsx
@@ -0,0 +1,13 @@
+import './style/login.scss'
+
+export default () => {
+    return (
+        <div class='login-fnd'>
+            <div class='login-form'>
+                <h1>شماره مجازی تورا</h1>
+                <span>ابتدا وارد شوید</span>
+                <button>ورود با تلگرام</button>
+            </div>
+        </div>
+    )
+}
