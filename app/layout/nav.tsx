1fde96ff4d5a97480fdd3f202ca16133c1f1df17
007 <dr007cc@gmail.com>
2024-04-22 01:16:31 +0330

    fix(app): update nav

--- app/layout/nav.tsx
+++ app/layout/nav.tsx
@@ -1,34 +1,44 @@
-import { A, useLocation } from '@solidjs/router'
+import { A } from '@solidjs/router'
 import { Component, Show } from 'solid-js'
 
 import './style/nav.scss'
 import { self } from 'store'
-import { UserIcon } from 'icons'
+import {
+    CirclePlusIcon,
+    HistoryIcon,
+    MailWarningIcon,
+    PhoneIcon,
+    UserIcon,
+} from 'icons'
 
 const Navbar: Component = () => {
-    const location = useLocation()
-
     return (
-        <nav class='navbar-fnd'>
-            <div class='avatar'>
-                <Show when={self.user.photo} fallback={<UserIcon />}>
-                    <img
-                        draggable={false}
-                        src={`/record/${self.user.id}.jpg`}
-                    />
-                </Show>
-            </div>
-
-            <div class='links'>
-                <A href='/' classList={{ active: location.pathname == '/' }}>
-                    Overview
+        <nav class='nav-fnd'>
+            <div class='left'>
+                <A href='/profile/' class='avatar'>
+                    <Show when={self.user.photo} fallback={<UserIcon />}>
+                        <img
+                            draggable={false}
+                            src={`/record/${self.user.id}.jpg`}
+                        />
+                    </Show>
                 </A>
-                <A href='/db/'>Database</A>
-                <A href='/map/'>Map</A>
-                <A href='/search/'>Search</A>
-                <A href='/config/'>Config</A>
+                <A href='/transactions/'>
+                    <HistoryIcon />
+                </A>
+                <A href='/messages/'>
+                    <MailWarningIcon />
+                </A>
+                <A href='/numbers/'>
+                    <PhoneIcon />
+                </A>
             </div>
-            <A href='/profile/' class='session' draggable={false}></A>
+            <div class='right'>
+                <A href='/profile/' class='money'>
+                    <span>{self.user.wallet.toLocaleString()}</span>
+                    <CirclePlusIcon />
+                </A>
+            </div>
         </nav>
     )
 }
