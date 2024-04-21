1652b2a6a63e88d0b1c21a951453a5e1734da0a3
007 <dr007cc@gmail.com>
2024-04-22 01:16:13 +0330

    feat(app): add phone and circle-plus icons

--- app/icons.tsx
+++ app/icons.tsx
@@ -135,3 +135,33 @@ export const MailOpenIcon = () => (
         <path d='m22 10-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 10' />
     </svg>
 )
+
+export const PhoneIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z' />
+    </svg>
+)
+
+export const CirclePlusIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <circle cx='12' cy='12' r='10' />
+        <path d='M8 12h8' />
+        <path d='M12 8v8' />
+    </svg>
+)
