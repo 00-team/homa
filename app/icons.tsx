2bc4b2b59001bf20bbb3c96376bcdf82e3bddf22
007 <dr007cc@gmail.com>
2024-04-22 00:48:39 +0330

    feat(app): add mail icons

--- app/icons.tsx
+++ app/icons.tsx
@@ -101,3 +101,35 @@ export const UserIcon = () => (
         <path d='M20 21a8 8 0 0 0-16 0' />
     </svg>
 )
+
+export const MailWarningIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='M22 10.5V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h12.5' />
+        <path d='m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7' />
+        <path d='M20 14v4' />
+        <path d='M20 22v.01' />
+    </svg>
+)
+
+export const MailOpenIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='M21.2 8.4c.5.38.8.97.8 1.6v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V10a2 2 0 0 1 .8-1.6l8-6a2 2 0 0 1 2.4 0l8 6Z' />
+        <path d='m22 10-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 10' />
+    </svg>
+)
