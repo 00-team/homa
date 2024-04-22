f09c758ff525bb0f9dde337b13fbe35a3fbf0467
007 <dr007cc@gmail.com>
2024-04-23 02:36:39 +0330

    feat(app): add needed icons for comps

--- app/icons.tsx
+++ app/icons.tsx
@@ -180,3 +180,77 @@ export const HomeIcon = () => (
         <polyline points='9 22 9 12 15 12 15 22'></polyline>
     </svg>
 )
+
+export const ChevronLeftIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='m15 18-6-6 6-6' />
+    </svg>
+)
+
+export const ChevronRightIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='m9 18 6-6-6-6' />
+    </svg>
+)
+
+export const ChevronDownIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='m6 9 6 6 6-6' />
+    </svg>
+)
+
+export const ChevronUpIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='m18 15-6-6-6 6' />
+    </svg>
+)
+
+export const TrashIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+    >
+        <path d='M3 6h18' />
+        <path d='M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6' />
+        <path d='M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2' />
+        <line x1='10' x2='10' y1='11' y2='17' />
+        <line x1='14' x2='14' y1='11' y2='17' />
+    </svg>
+)
