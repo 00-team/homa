f51417a4ac5c0a41afff1559fae847296cb8c9e9
007 <dr007cc@gmail.com>
2024-04-23 18:27:59 +0330

    feat(app): show the select state in home

--- app/layout/home.tsx
+++ app/layout/home.tsx
@@ -2,8 +2,19 @@ import './style/home.scss'
 import { COUNTRY_LIST } from './country-list'
 import { SERVICE_LIST } from './service-list'
 import { Select } from 'comps'
+import { createStore } from 'solid-js/store'
 
 export default () => {
+    type State = {
+        country: number | null
+        service: string | null
+    }
+
+    const [state, setState] = createStore<State>({
+        country: null,
+        service: null,
+    })
+
     return (
         <div class='home-fnd'>
             <div class='country'>
@@ -14,16 +25,34 @@ export default () => {
                             <span class='cc'>+{c[1]}</span>
                         </div>
                     ))}
-                    onChange={v => console.log(v)}
+                    onChange={v => setState({ country: COUNTRY_LIST[v[0]][0] })}
                     placeholder='کشور'
                 />
             </div>
             <div class='service'>
                 <Select
-                    items={SERVICE_LIST.map(s => s[1])}
-                    onChange={v => console.log(SERVICE_LIST[v[0]])}
+                    items={SERVICE_LIST.map(s => (
+                        <span
+                            class='service-dpy'
+                            style={
+                                s[2]
+                                    ? { direction: 'rtl' }
+                                    : {
+                                          direction: 'ltr',
+                                          'font-family': 'var(--en)',
+                                      }
+                            }
+                        >
+                            {s[2] || s[1]}
+                        </span>
+                    ))}
+                    onChange={v => setState({ service: SERVICE_LIST[v[0]][0] })}
                     placeholder='سرویس'
                 />
+            </div>
+            <div style={{ display: 'flex', 'flex-direction': 'column' }}>
+                <span>country: {state.country}</span>
+                <span>service: {state.service}</span>
             </div>
         </div>
     )
