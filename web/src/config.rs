7c601f7656ad0a6940d963db67a0e59d1c50fcd2
007 <dr007cc@gmail.com>
2024-04-26 07:55:25 +0330

    feat(web): add va_apikey in config

--- web/src/config.rs
+++ web/src/config.rs
@@ -1,9 +1,10 @@
-use std::{env, sync::OnceLock};
 use sha2::{Digest, Sha256};
+use std::{env, sync::OnceLock};
 
 #[derive(Debug)]
 pub struct Config {
     pub bot_token_hash: [u8; 32],
+    pub va_apikey: String,
 }
 
 impl Config {
@@ -18,6 +19,9 @@ pub fn config() -> &'static Config {
     static STATE: OnceLock<Config> = OnceLock::new();
     STATE.get_or_init(|| {
         let token = Sha256::digest(&env::var("TELOXIDE_TOKEN").unwrap());
-        Config { bot_token_hash: token.into() }
+        Config {
+            bot_token_hash: token.into(),
+            va_apikey: env::var("VA_APIKEY").unwrap(),
+        }
     })
 }
