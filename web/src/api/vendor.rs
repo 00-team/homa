2b5b350061cba6d255905a8c8803ec5036fbc8f2
007 <dr007cc@gmail.com>
2024-04-30 02:22:27 +0330

    fix(web): update check-prices

--- web/src/api/vendor.rs
+++ web/src/api/vendor.rs
@@ -2,22 +2,22 @@ use std::collections::HashMap;
 use std::ops::Deref;
 use std::sync::Mutex;
 
-use actix_web::web::{Json, Path, Query};
+use actix_web::web::{Json, Path};
 use actix_web::{get, post, HttpResponse, Scope};
 use lazy_static::lazy_static;
 use serde::Deserialize;
-use serde_json::Value;
-use utoipa::{IntoParams, OpenApi, ToSchema};
+use utoipa::{OpenApi, ToSchema};
 
 use crate::config::config;
 use crate::docs::UpdatePaths;
 use crate::models::{AppErr, AppErrForbidden, Response, User};
-use crate::utils::send_webhook;
+use crate::utils;
 use crate::vendor;
 
+type Prices = HashMap<String, (i64, i64)>;
 lazy_static! {
-    static ref PRICES: Mutex<HashMap<String, i64>> = Mutex::new(HashMap::new());
-    static ref PRICES_UPDATE: i64 = 0;
+    static ref PRICES: Mutex<Prices> = Mutex::new(HashMap::new());
+    static ref PRICES_UPDATE: Mutex<i64> = Mutex::new(0);
 }
 
 // type Prices = HashMap<String, i64>;
@@ -42,43 +42,47 @@ pub struct ApiDoc;
 
 #[utoipa::path(get, responses((status = 200, body = User)))]
 #[get("/prices/")]
-async fn prices_get(_: User) -> Response<HashMap<String, i64>> {
-    let mut x = PRICES.lock().unwrap();
-    let len = x.len();
-    x.insert(format!("hi-{}", len), 12);
+async fn prices_get(_: User) -> Response<Prices> {
+    let x = PRICES.lock().unwrap();
     Ok(Json(x.deref().clone()))
 }
 
-#[derive(Debug, Deserialize, ToSchema, IntoParams)]
-struct CheckServiceQuery {
-    time: u16,
-    service: String,
-}
-
-#[utoipa::path(
-    get,
-    params(CheckServiceQuery),
-    responses((status = 200))
-)]
+#[utoipa::path(get, responses((status = 200)))]
 #[get("/check-service/")]
-async fn check_service(
-    _: User, q: Query<CheckServiceQuery>,
-) -> Response<Value> {
-    if q.time == 20 {
-        let args = vec![("service", q.service.as_str())];
-        let result = vendor::request("getPricesVerification", args).await?;
-        Ok(Json(result))
-    } else {
-        let time = q.time.to_string();
-        let args = vec![("rent_time", time.as_str())];
-        let result =
-            vendor::request("getRentServicesAndCountries", args).await?;
-        Ok(Json(result))
+async fn check_service(_: User) -> Response<Prices> {
+    let now = utils::now();
+    let mut update = PRICES_UPDATE.lock().unwrap();
+    let mut prices = PRICES.lock().unwrap();
+    if *update + 600 < now {
+        let result = vendor::request("getPrices", vec![]).await?;
+        prices.clear();
+        *update = now;
+
+        let result = if let Some(v) = result.as_object() {
+            v
+        } else {
+            return Err(AppErr::default());
+        };
+
+        result.iter().for_each(|(country, v)| {
+            v.as_object().unwrap().iter().for_each(|(service, vv)| {
+                let vv = vv.as_object().unwrap();
+                let count = vv.get("count").unwrap().as_i64().unwrap();
+                let cost = vv.get("cost").unwrap().as_i64().unwrap();
+
+                if count == 0 {
+                    return;
+                }
+
+                prices.insert(format!("{country}-{service}"), (cost, count));
+            });
+        });
     }
+
+    Ok(Json(prices.deref().clone()))
 }
 
 #[derive(Deserialize, ToSchema)]
-// #[allow(non_snake_case)]
 #[serde(rename_all = "camelCase")]
 struct SmsData {
     activation_id: i64,
@@ -103,7 +107,7 @@ async fn sms_callback(
         return Err(AppErrForbidden("invalid pass"));
     }
 
-    send_webhook(
+    utils::send_webhook(
         "Sms",
         &format!(
             "
