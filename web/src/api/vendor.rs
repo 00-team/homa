979247bf499d7c22cc8037327cd18dc7668986d3
007 <dr007cc@gmail.com>
2024-05-18 07:57:17 +0330

    fix(web): update vendor api

--- web/src/api/vendor.rs
+++ web/src/api/vendor.rs
@@ -1,26 +1,17 @@
 use std::collections::HashMap;
-use std::ops::Deref;
-use std::sync::Mutex;
 
 use actix_web::web::{Data, Json, Path};
 use actix_web::{get, post, HttpResponse, Scope};
-use lazy_static::lazy_static;
 use serde::Deserialize;
 use utoipa::{OpenApi, ToSchema};
 
 use crate::config::{config, Config};
 use crate::docs::UpdatePaths;
-use crate::general::{general_get, general_set};
+use crate::general::{general_get, general_set, PriceValue};
 use crate::models::user::User;
 use crate::models::{AppErr, AppErrForbidden, Response};
 use crate::vendor::{self, rub_irr_price};
 use crate::{utils, AppState};
-
-type Prices = HashMap<String, (f64, i64)>;
-lazy_static! {
-    static ref PRICES: Mutex<Prices> = Mutex::new(HashMap::new());
-    static ref PRICES_UPDATE: Mutex<i64> = Mutex::new(0);
-}
 
 #[derive(OpenApi)]
 #[openapi(
@@ -32,24 +23,33 @@ lazy_static! {
 )]
 pub struct ApiDoc;
 
+type Prices = HashMap<String, i64>;
+
 #[utoipa::path(get, responses((status = 200)))]
 #[get("/prices/")]
 async fn prices(_: User, state: Data<AppState>) -> Response<Prices> {
     let now = utils::now();
-    let mut update = PRICES_UPDATE.lock().expect("PRICES_UPDATE lock err");
-    let mut prices = PRICES.lock().expect("PRICES lock err");
-    if *update + 600 < now {
-        *update = now;
+    let mut general = general_get(&state.sql).await?;
+    let mut update_general = false;
+
+    if general.rub_irr_update + 86400 < now {
+        update_general = true;
+        general.rub_irr_update = now;
+        general.rub_irr = rub_irr_price().await?;
+    }
+
+    if general.prices_update + 600 < now {
+        update_general = true;
+        general.prices_update = now;
+
+        let avg_diff =
+            if general.price_diff_count != 0 && general.price_diff_total != 0 {
+                (general.price_diff_total / general.price_diff_count) as f64
+            } else {
+                10.0
+            };
 
         let result = vendor::request("getPrices", vec![]).await?;
-        prices.clear();
-
-        let mut general = general_get(&state.sql).await?;
-        if general.rub_irr_update + 86400 < now {
-            general.rub_irr_update = now;
-            general.rub_irr = rub_irr_price().await?;
-            general_set(&state.sql, &general).await?;
-        }
 
         result.as_object().expect("result is not an object").iter().for_each(
             |(country, v)| {
@@ -65,20 +65,47 @@ async fn prices(_: User, state: Data<AppState>) -> Response<Prices> {
                             return;
                         }
 
-                        let price = cost * general.rub_irr as f64 * Config::TAX;
-                        let price = ((price / 1e4).ceil() * 1e4).max(15e4);
-
-                        prices.insert(
-                            format!("{country}-{service}"),
-                            (price, count),
-                        );
+                        let key = format!("{country}-{service}");
+                        if let Some(p) = general.prices.get_mut(&key) {
+                            p.count = count;
+                            p.cost_api = cost + avg_diff;
+                        } else {
+                            general.prices.insert(
+                                key,
+                                PriceValue {
+                                    cost_api: cost,
+                                    count,
+                                    ..Default::default()
+                                },
+                            );
+                        }
                     },
                 );
             },
         );
     }
 
-    Ok(Json(prices.deref().clone()))
+    if update_general {
+        general_set(&state.sql, &general).await?;
+    }
+
+    let prices: Prices = general
+        .prices
+        .iter()
+        .map(|(k, v)| {
+            let cost = if v.cost_buy > 0.0 && v.timestamp + 30 * 86400 > now {
+                v.cost_buy
+            } else {
+                v.cost_api
+            };
+
+            let p = cost * general.rub_irr as f64 * Config::TAX;
+            let p = ((p / 1e4).ceil() * 1e4).max(15e4) as i64;
+            (k.clone(), p)
+        })
+        .collect();
+
+    Ok(Json(prices))
 }
 
 #[derive(Deserialize, ToSchema)]
