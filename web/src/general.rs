1cf9124594bd0c188bc15224500f150146703f07
007 <dr007cc@gmail.com>
2024-05-18 06:38:15 +0330

    fix(web): update general

--- web/src/general.rs
+++ web/src/general.rs
@@ -1,6 +1,10 @@
+use std::collections::HashMap;
+
 use sqlx::{Pool, Sqlite};
 
-use crate::models::AppErr;
+use crate::models::{AppErr, JsonStr};
+
+type PriceData = HashMap<String, i64>;
 
 #[derive(sqlx::FromRow)]
 pub struct General {
@@ -8,6 +12,25 @@ pub struct General {
     pub total_money: i64,
     pub rub_irr: i64,
     pub rub_irr_update: i64,
+    pub price_diff_total: i64,
+    pub price_diff_count: i64,
+    pub prices: JsonStr<PriceData>,
+    pub prices_update: i64,
+}
+
+impl Default for General {
+    fn default() -> Self {
+        Self {
+            available_money: 0,
+            total_money: 0,
+            rub_irr: 0,
+            rub_irr_update: 0,
+            price_diff_total: 0,
+            price_diff_count: 0,
+            prices: JsonStr(PriceData::new()),
+            prices_update: 0,
+        }
+    }
 }
 
 pub async fn general_get(pool: &Pool<Sqlite>) -> Result<General, AppErr> {
@@ -27,12 +50,7 @@ pub async fn general_get(pool: &Pool<Sqlite>) -> Result<General, AppErr> {
             .execute(pool)
             .await;
 
-            Ok(General {
-                available_money: 0,
-                total_money: 0,
-                rub_irr: 0,
-                rub_irr_update: 0,
-            })
+            Ok(General::default())
         }
     }
 }
@@ -50,11 +68,12 @@ pub async fn general_set(
     match result {
         Some(_) => {
             sqlx::query! {
-                "update general set
-                available_money = ?, total_money = ?,
-                rub_irr = ?, rub_irr_update = ?",
-                general.available_money, general.total_money,
-                general.rub_irr, general.rub_irr_update
+                "update general set available_money = ?, total_money = ?,
+                rub_irr = ?, rub_irr_update = ?, price_diff_total = ?,
+                price_diff_count = ?, prices = ?, prices_update = ?",
+                general.available_money, general.total_money, general.rub_irr,
+                general.rub_irr_update, general.price_diff_total, 
+                general.price_diff_count, general.prices, general.prices_update
             }
             .execute(pool)
             .await?;
@@ -63,9 +82,12 @@ pub async fn general_set(
         }
         None => {
             sqlx::query! {
-                "insert into general values(?, ?, ?, ?)",
-                general.available_money, general.total_money,
-                general.rub_irr, general.rub_irr_update
+                "insert into general(available_money, total_money, rub_irr,
+                rub_irr_update, price_diff_total, price_diff_count, prices,
+                prices_update) values(?,?,?,?,?,?,?,?)",
+                general.available_money, general.total_money, general.rub_irr,
+                general.rub_irr_update, general.price_diff_total, 
+                general.price_diff_count, general.prices, general.prices_update
             }
             .execute(pool)
             .await?;
