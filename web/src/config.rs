a4a7445efbb3527a21eb551ee059c0daa0d7e638
007 <dr007cc@gmail.com>
2024-05-24 12:22:48 +0330

    feat(web): add bot token in config

--- web/src/config.rs
+++ web/src/config.rs
@@ -3,6 +3,7 @@ use std::{env, sync::OnceLock};
 
 #[derive(Debug)]
 pub struct Config {
+    pub bot_token: String,
     pub bot_token_hash: [u8; 32],
     pub discord_webhook: String,
     pub sms_cb_pass: String,
@@ -18,9 +19,10 @@ impl Config {
 pub fn config() -> &'static Config {
     static STATE: OnceLock<Config> = OnceLock::new();
     STATE.get_or_init(|| {
-        let token = Sha256::digest(&env::var("TELOXIDE_TOKEN").unwrap());
+        let bot_token = env::var("TELOXIDE_TOKEN").unwrap();
         Config {
-            bot_token_hash: token.into(),
+            bot_token_hash: Sha256::digest(&bot_token).into(),
+            bot_token,
             discord_webhook: env::var("DISCORD_WEBHOOK").unwrap(),
             sms_cb_pass: env::var("SMS_CB_PASS").unwrap(),
             navasan_apikey: env::var("NAVASAN_APIKEY").unwrap(),
