b1128cb6ea25536cbb9b90d267d4e65d929e9629
007 <dr007cc@gmail.com>
2024-05-27 10:50:19 +0330

    fix(app): add extra in wallet

--- app/layout/home.tsx
+++ app/layout/home.tsx
@@ -89,7 +89,7 @@ export default () => {
 
     function buy() {
         if (selected() > self.user.wallet) {
-            nav('/profile/?add=' + (selected() - self.user.wallet))
+            nav('/profile/?add=' + (selected() - self.user.wallet + 7e4))
             return
         }
 
