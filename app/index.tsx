0a3f41c555397a037c9e01b39149f864fe23e5e1
007 <dr007cc@gmail.com>
2024-09-01 19:37:35 +0330

    feat: init shop

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ import 'style/index.scss'
 import Login from 'layout/login'
 import Dash from 'layout/dash'
 import Home from 'layout/home'
+import Shop from 'layout/shop'
 import Profile from 'layout/profile'
 import Alert from 'comps/alert'
 import { Route, Router } from '@solidjs/router'
@@ -18,7 +19,7 @@ const Root = () => (
         <Show when={self.loged_in} fallback={<Login />}>
             <Router>
                 <Route path='/' component={Dash}>
-                    <Route path='/' component={Home} />
+                    <Route path='/' component={Shop} />
                     <Route path='/profile/' component={Profile} />
                     <Route path='/messages/' component={Messages} />
                     <Route path='/messages/:page' component={Messages} />
---
+++ app/layout/shop.tsx
@@ -0,0 +1,5 @@
+import './style/shop.scss'
+
+export default () => {
+    return <div class='shop-fnd'>shop</div>
+}
