2ee87c8f9a337f0a3732e560e9d8e51b71247147
007 <dr007cc@gmail.com>
2024-09-02 02:45:02 +0330

    fix: update stars

--- app/shop/stars.tsx
+++ app/shop/stars.tsx
@@ -1,10 +1,35 @@
+import { createStore } from 'solid-js/store'
 import './style/stars.scss'
+import { TomanDpy, httpx } from 'shared'
+import { onMount } from 'solid-js'
 
 export default () => {
+    type State = {
+        price: number
+    }
+    const [state, setState] = createStore<State>({
+        price: 0,
+    })
+
+    onMount(load)
+
+    function load() {
+        httpx({
+            url: '/api/stars/price/',
+            method: 'GET',
+            onLoad(x) {
+                if (x.status != 200) return
+                setState({ price: x.response })
+            },
+        })
+    }
+
     return (
         <div class='shop-stars-fnd'>
             {[50, 75, 100, 150, 250, 350, 500, 750, 1000, 1500, 2500].map(s => (
-                <div>{s}</div>
+                <div>
+                    {s} | <TomanDpy irr={s * state.price} /> تومان
+                </div>
             ))}
         </div>
     )
